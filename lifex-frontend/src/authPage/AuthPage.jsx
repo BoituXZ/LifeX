23f71fe99fa576995d8135229f210e41b666af98
Boitumelo <167770627+BoituXZ@users.noreply.github.com>
2024-10-24 11:45:40 +0200

    Changes

---
+++ lifex-frontend/src/authPage/AuthPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './AuthPage.css'
+const AuthPage = () => {
+  return (
+    <>
+        <div className="navBar">
+            <div className="navBar_logo">Life<span>X</span></div>
+            <div className="navBar_center">
+                <div className="navBar_center_item">Mission</div>
+                <div className="navBar_center_item">About Us</div>
+                <div className="navBar_center_item">Contact us</div>
+            </div>
+        </div>
+        <div className="textbox">
+            
+        </div>
+
+
+    </>
+  )
+}
+
+export default AuthPage;
