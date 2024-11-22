16f13362db0e55af181b464a43de17cb347fa507
Boitumelo <167770627+BoituXZ@users.noreply.github.com>
2024-11-22 16:46:48 +0200

    Refactor CSS styles: remove unused styles, update font imports, and enhance layout structure for improved design consistency

--- lifex-frontend/src/webpages/pills/Pills.jsx
+++ lifex-frontend/src/webpages/pills/Pills.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navigation from '../../components/Navigation';
+
 import styles from './Pills.module.css';  
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getDatabase, ref, push, onValue } from 'firebase/database';
@@ -9,9 +10,319 @@ import { getDatabase, ref, push, onValue } from 'firebase/database';
 const Pills = () => {
   return(
     <>
-    <div className="pageContainer">
-      <Navigation/>
-    </div>
+      <div className={styles.pageContainer}>
+        <section className={styles.nav}>Nav</section>
+
+        <section className={styles.centreSection}>
+          <h1>Your Pills</h1>
+          <div className={styles.pillsBody}>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div><div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div><div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                <h3>Ibuprofen</h3>
+                <h3>500mg</h3>
+              </div>
+              <div className={styles.cardBottom}>
+                <div className={styles.cardBottomLeft}>
+                  <p>Take Two Times a Day</p>
+                  <p>12/08/2024</p><br/>
+                  <p>Pharamacy:     <br/>12Ave</p><br/>
+                  <p>Doctor: Boitumelo</p>
+                </div>
+                <div className={styles.cardBottomRight}>
+                  
+                  <button className={styles.cardButton}>Taken</button>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.rightSection}>Profile</section>
+      </div>
     </>
     
   )  
